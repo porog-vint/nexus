fb3da7c48ad71b68100180a18d9716bf4370db25
il141 <il141962@gmail.com>
2025-11-29 08:40:40 +0300

    Инициализация проекта, добавлен Main класс

---
+++ src/main/java/at/nexus/core/Main.java
@@ -0,0 +1,16 @@
+package at.nexus.core;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+    private static final Logger log = LoggerFactory.getLogger(Main.class);
+
+    static void main(String[] args) {
+        log.info("Запуск Nexus Voice AI...");
+
+        // TODO: Инициализация компонентов
+
+        log.info("Nexus Voice AI готов к работе");
+    }
+}
